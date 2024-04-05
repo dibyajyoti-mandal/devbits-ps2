a92a5831717f1de4a7559fadab0ffb2edfe3f59e
cookie_jar <cptpricecod098@gmail.com>
2024-04-05 18:50:58 +0530

    message is clear

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,16 @@
-console.log("hrrr")
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import connectDB from "./util/dbconnection.js"
 
+const app=express()
+dotenv.config()
+
+const port = process.env.PORT||3000;
+connectDB()
+
+
+app.listen(port,()=>{
+    console.log("Running on port", port);
+})
 
