37ae974b7e71e2bf06e7aef1fc2ea5edb911f38c
amaan1203 <127188069+amaan1203@users.noreply.github.com>
2024-04-06 14:19:02 +0530

    first commit in frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+function App() {
+  return (
+    <>
+     {/* <Grid/> */}
+      <div className="App h-[100vh] w-[100vw] bg-black">
+       <Navbar />
+    </div>
+    </>
+    
+  );
+}
+
+export default App;
