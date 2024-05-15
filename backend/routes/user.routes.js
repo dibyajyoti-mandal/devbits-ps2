e006344d588fd7b9c7d68dc244a01217082ff996
cookie_jar <cptpricecod098@gmail.com>
2024-05-15 12:05:28 +0530

    h

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getUser, test, updateUser} from "../controllers/user.js"
+import {enroll, getUser, test, unEnroll, updateUser} from "../controllers/user.js"
 import { verifyToken } from "../util/verifyToken.js";
 
 const router = express.Router()
@@ -7,6 +7,8 @@ const router = express.Router()
 router.get("/r", test)
 router.get("/find/:id", getUser)
 router.put("/:id", verifyToken, updateUser)
+router.put("/enroll/:id", verifyToken, enroll)
+router.put("/unenroll/:id", verifyToken, unEnroll)
 
 
 export default router 
